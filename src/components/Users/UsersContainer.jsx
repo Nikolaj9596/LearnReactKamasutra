e9d3ada467fff2400bb179a459676ad61139f707
Nikolaj <nikolaj.v@itfox-web.com>
2023-07-31 22:04:53 +0300

    feat: added a new event hendler

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/user-reducer";
+import {
+  followAC,
+  setUsersAC,
+  unfollowAC,
+  setCurrentPageAC,
+  setTotalUsersCountAC,
+} from "../../redux/user-reducer";
 import Users from "./Users";
-
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage
-  }
-}
+    currentPage: state.usersPage.currentPage,
+  };
+};
 
 let mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
-      dispatch(followAC(userId))
+      dispatch(followAC(userId));
     },
     unfollow: (userId) => {
-      dispatch(unfollowAC(userId))
+      dispatch(unfollowAC(userId));
     },
     setUsers: (users) => {
-      dispatch(setUsersAC(users))
-    }
-  }
-}
-export default connect(
-  mapStateToProps, mapDispatchToProps
-)(Users);
+      dispatch(setUsersAC(users));
+    },
+
+    setCurrentPage: (currentPage) => {
+      dispatch(setCurrentPageAC(currentPage));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalUsersCountAC(totalCount));
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
